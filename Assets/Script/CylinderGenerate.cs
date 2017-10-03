91f87edc62f35081b8e640d07704c853243b8ef8
joshua chung <joshua.chung@ocsbstudent.ca>
2017-10-03 09:54:47 -0400

    added cylinder

---
+++ Assets/Script/CylinderGenerate.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CylinderGenerate : MonoBehaviour {
+    public float generateSpeed=0.2f;
+    public GameObject layer;
+    float timer = 0f;
+    float height = 0f;
+
+	// Use this for initialization
+	void Start () {
+       /* for(int i = 0; i < 100; i++)
+        {
+            Vector3 pos = new Vector3(0, height, 0);
+            Quaternion rotation = new Quaternion(0, 0, 0, 1);
+            Instantiate(layer, pos, rotation);
+            height += 10;
+            timer = 0;
+        }*/
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        timer += Time.deltaTime;
+
+        if (timer >=generateSpeed)
+        {
+            Vector3 pos = new Vector3(0,height,0);
+            Quaternion rotation = new Quaternion(0,0,0,1);
+            Instantiate(layer,pos,rotation);
+            height += 10;
+            timer = 0;
+        }
+	}
+}
---
+++ Assets/Script/CylinderScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CylinderScript : MonoBehaviour {
+    public float destroyTime = 2f;
+    float timer = 0f;
+    public Material[] materials;
+    Renderer[] rend;
+    
+   GameObject[] Children=new GameObject[10];
+    // Use this for initialization
+    void Start () {
+        int counter=1;
+        rend = GetComponentsInChildren<Renderer>();
+
+         for (int i=0;i< rend.Length;i++)
+         {
+            print(counter);
+            counter++;
+            Children[i]=(rend[i].gameObject);
+            rend[i].material = materials[Random.Range(0, 3)]; 
+         }
+
+
+        Destroy(this.gameObject,destroyTime);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        timer += Time.deltaTime;
+
+        
+
+	}
+}
